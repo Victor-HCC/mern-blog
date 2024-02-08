59943df7d7d7fd611cdc0f6eb9a84e6cc17fc29c
Victor <vicktorcori@hotmail.com>
2024-02-08 20:41:13 -0300

    Add functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
+  
   if(!username || !email || !password || username === '' || email === '' || password === '') {
     next(errorHandler(400, 'All fields are required'));
   }
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
     email, 
     password: hashedPassword
   })
-
+  
   try {
     await  newUser.save();
     res.json({ message: 'Signup successful'})
